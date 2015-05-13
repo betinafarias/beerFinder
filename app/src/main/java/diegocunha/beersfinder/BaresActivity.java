7e51527decf197be252442479b73b414b3f0c3c6
Diego Cunha <domdiegobc@gmail.com>
2015-05-13 11:56:18 -0300

    - Adicionado valores ao Spinner; - Função que manda parâmetros para busca;

--- app/src/main/java/diegocunha/beersfinder/BaresActivity.java
+++ app/src/main/java/diegocunha/beersfinder/BaresActivity.java
@@ -7,8 +7,11 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.parse.FindCallback;
@@ -21,15 +24,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class BaresActivity extends ActionBarActivity {
+public class BaresActivity extends ActionBarActivity{
 
-    List<String> listaBares;
-    //ArrayAdapter<String> dataAdapter;
     Spinner mySpinner;
     ProgressDialog mProgressDialog;
     ConnectivityManager conectivtyManager;
     boolean isOn;
-    String nomeBar, AppID, ClientID;
+    String AppID, ClientID;
+    private String nomeBar[];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,13 +42,10 @@ public class BaresActivity extends ActionBarActivity {
         ClientID = getString(R.string.ClientID);
         Parse.initialize(this, AppID, ClientID);
 
+        setContentView(R.layout.activity_bares);
         mySpinner = (Spinner)findViewById(R.id.meuSpinner);
         loadBares();
-
-        setContentView(R.layout.activity_bares);
     }
-
-
 
     /**********************************************
      * Autores: Diego Cunha Gabriel Cataneo    ****
@@ -78,60 +77,41 @@ public class BaresActivity extends ActionBarActivity {
      * Função: void loadBares                  ****
      * Funcionalidade: Preenche o Spinner      ****
      **********************************************/
-    public void loadBares()
-    {
-
-        listaBares = new ArrayList<String>();
-        final ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, listaBares);
-
-
-        try
-        {
-            mProgressDialog =  new ProgressDialog(this);
+   public void loadBares()
+   {
+       try
+       {
+            mProgressDialog = new ProgressDialog(this);
             mProgressDialog.setCanceledOnTouchOutside(true);
             mProgressDialog.setCancelable(true);
             mProgressDialog.setTitle("Carregando");
-            mProgressDialog.setMessage("Loading. . . ");
-            mProgressDialog.show();
+            mProgressDialog.setMessage("Loading . . .");
 
-            if(verificaConexao())
-            {
-                ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("Bares");
-                query.findInBackground(new FindCallback<ParseObject>() {
-                    @Override
-                    public void done(List<ParseObject> list, ParseException e) {
-                        if (e == null) {
-                            if (list.size() > 0) {
-                                for (int i = 0; i < list.size(); i++) {
-                                    ParseObject pObject = list.get(i);
-                                    nomeBar = pObject.getString("NomeBar");
-                                    listaBares.add(nomeBar);
-                                    mProgressDialog.dismiss();
-                                    dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                                    mySpinner.setAdapter(dataAdapter);
-                                }
+            this.nomeBar = new String[]{"Dublin", "Mulligan", "Natalicio", "Soccer Point", "Thomas"};
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, nomeBar);
 
-                            } else {
-                                Toast.makeText(getApplicationContext(), "Lista vazia", Toast.LENGTH_SHORT).show();
-                            }
-                        } else {
-                            Toast.makeText(getApplicationContext(), e.getMessage().toString(), Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-
-
-            }
-            else
-            {
-                Toast.makeText(getApplicationContext(), "Sem acesso a internet", Toast.LENGTH_SHORT).show();
-            }
-        }
-        catch (Exception ex)
-        {
+            mySpinner.setAdapter(adapter);
+       }
+       catch (Exception ex)
+       {
+           ex.printStackTrace();
+           Toast.makeText(getApplicationContext(), ex.getMessage().toString(), Toast.LENGTH_SHORT).show();
+       }
+       finally
+       {
             mProgressDialog.dismiss();
-            ex.printStackTrace();
-        }
+       }
+   }
+
+    /******************************************************
+     * Autores: Diego Cunha Gabriel Cataneo            ****
+     * Criação: 13/05/2015                             ****
+     * Função: void sendBares                          ****
+     * Funcionalidade: Manda o parametro de busca      ****
+     ******************************************************/
+    public void sendBares(View view)
+    {
+
     }
 
     @Override
