754a72c1883eae5d52ddd40c1365c7e839934743
Diego Cunha <domdiegobc@gmail.com>
2015-05-24 16:19:53 -0300

    - Terminado myIntineraire; - Implementado em HowtoGo;

--- app/src/main/java/diegocunha/beersfinder/HowtoGoActivity.java
+++ app/src/main/java/diegocunha/beersfinder/HowtoGoActivity.java
@@ -11,14 +11,18 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polygon;
 import com.google.android.gms.maps.model.PolygonOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.w3c.dom.Document;
 
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -28,24 +32,25 @@ public class HowtoGoActivity extends FragmentActivity {
     myLocation MeuLugar;
     private double Latitude, Longitude, barLat, barLng;
     private String strLat, strLong, strBarLat, strBarLng;
-
+    myIntineraire md;
+    LatLng start, end;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         Bundle extras = getIntent().getExtras();
         MeuLugar = new myLocation(this);
-        mMap = ((MapFragment)getFragmentManager().findFragmentById(R.id.map)).getMap();
+        md = new myIntineraire();
+
+        setContentView(R.layout.activity_howtogo);
+        setUpMapIfNeeded();
+
         if(extras != null)
         {
             barLat = extras.getDouble("Latitude");
             barLng = extras.getDouble("Longitude");
             connect();
         }
-
-        setContentView(R.layout.activity_howtogo);
-        setUpMapIfNeeded();
-
     }
 
     public void connect()
@@ -59,6 +64,22 @@ public class HowtoGoActivity extends FragmentActivity {
             strLong = String.valueOf(Longitude);
             strBarLat = String.valueOf(barLat);
             strBarLng = String.valueOf(barLng);
+
+            start = new LatLng(Latitude, Longitude);
+            end  = new LatLng(barLat, barLng);
+
+            Document doc = md.getDocument(start, end, myIntineraire.MODE_DRIVING);
+
+            ArrayList<LatLng> directPoint = md.getDirection(doc);
+            PolylineOptions rectline = new PolylineOptions().width(3).color(Color.GREEN);
+
+            for(int i = 0; i <directPoint.size();i++)
+            {
+                rectline.add(directPoint.get(i));
+            }
+
+            Polyline polyline = mMap.addPolyline(rectline);
+
         }
         catch (Exception ex)
         {
@@ -79,9 +100,11 @@ public class HowtoGoActivity extends FragmentActivity {
             // Try to obtain the map from the SupportMapFragment.
             mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
                     .getMap();
+            //connect();
             // Check if we were successful in obtaining the map.
             if (mMap != null) {
                 setUpMap();
+                //connect();
             }
         }
     }
